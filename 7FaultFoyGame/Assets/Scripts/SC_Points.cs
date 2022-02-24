bc766e85846af79d50da664f91b959a53dca93a5
Flgjfvevdk <74116706+Flgjfvevdk@users.noreply.github.com>
2022-02-24 17:19:33 +0100

    SystemePoint + importation

---
+++ 7FaultFoyGame/Assets/Scripts/SC_Points.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SC_Points : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float point;
+    public Text textPoint;
+    void Start()
+    {
+        point = 0.0f;
+        textPoint.text = "Points : 0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    addPoint(10);
+        //}
+    }
+
+    public void addPoint(float value)
+    {
+        point += value;
+        textPoint.text = "Points : " + point;
+    }
+}
